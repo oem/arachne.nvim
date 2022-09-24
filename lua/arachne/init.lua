d02d624e960dbe782a49d5d910618527faac4ec3
Ömür Özkir <oemuer.oezkir@gmail.com>
2022-09-24 10:44:49 +0200

    opening the file (constructed file name)

--- lua/arachne/init.lua
+++ lua/arachne/init.lua
@@ -6,8 +6,11 @@ M.new = function(name, tags)
     local date_prefix = os.date("%Y-%m-%d")
     local file_tags = table.concat(tags, "_")
     local file_name = date_prefix .. "--" .. name .. "__" .. file_tags .. ".md"
+    vim.cmd("e " .. file_name)
 end
 
 M.rename = function() end
+
+M.setup = function() end
 
 return M
