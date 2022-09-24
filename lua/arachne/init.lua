3238ae836fd454512f35b6ba2ef8f67142257649
Ömür Özkir <oemuer.oezkir@gmail.com>
2022-09-24 17:06:53 +0200

    properly normalize title to use in the file name while using the title as first line in the new markdown file

--- lua/arachne/init.lua
+++ lua/arachne/init.lua
@@ -4,14 +4,21 @@ M.options = {}
 
 local defaults = {notes_directory = os.getenv("HOME") .. "/notes"}
 
-M.new = function(name, tags)
+M.new = function() end
+
+M.open = function(name, tags)
     local date_prefix = os.date("%Y-%m-%d")
+
+    local normalized_name = string.lower(name)
+    normalized_name = string.gsub(normalized_name, " +", "-")
+
     local file_tags = table.concat(tags, "_")
     local postfix = ""
     if file_tags ~= "" then postfix = "__" .. file_tags end
-    local file_name = date_prefix .. "--" .. name .. postfix .. ".md"
 
+    local file_name = date_prefix .. "--" .. normalized_name .. postfix .. ".md"
     vim.cmd("e " .. M.options.notes_directory .. "/" .. file_name)
+    vim.api.nvim_put({"# " .. name}, "l", false, true)
 end
 
 M.rename = function() end
