e7d536e1ec9a34f53277103d382420a350243859
Ömür Özkir <oemuer.oezkir@gmail.com>
2022-09-24 14:44:26 +0200

    building a full path for a new note, which is also configurable

--- lua/arachne/init.lua
+++ lua/arachne/init.lua
@@ -1,16 +1,23 @@
 M = {}
 
--- User can set a notes directory that we can use here
--- The fileformat should probably also be configurable, but we will start with markdown as default
+M.options = {}
+
+local defaults = {notes_directory = os.getenv("HOME") .. "/notes"}
+
 M.new = function(name, tags)
     local date_prefix = os.date("%Y-%m-%d")
     local file_tags = table.concat(tags, "_")
-    local file_name = date_prefix .. "--" .. name .. "__" .. file_tags .. ".md"
-    vim.cmd("e " .. file_name)
+    local postfix = ""
+    if file_tags ~= "" then postfix = "__" .. file_tags end
+
+    local file_name = date_prefix .. "--" .. name .. postfix .. ".md"
+    vim.cmd("e " .. M.options.notes_directory .. "/" .. file_name)
 end
 
 M.rename = function() end
 
-M.setup = function() end
+M.setup = function(opts)
+    M.options = vim.tbl_deep_extend("force", {}, defaults, opts or {})
+end
 
 return M
